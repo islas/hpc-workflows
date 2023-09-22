7eae00dc3b65fd73d8a4089ca2235861d0aae050
Anthony Islas <aislas@ucar.edu>
2023-09-22 15:34:04 -0600

    Rewriting results blocking to be a poll instead of last step

--- .ci/Test.py
+++ .ci/Test.py
@@ -1,11 +1,14 @@
 import os
 import sys
 import json
+import time
 
 from SubmitAction  import SubmitAction
 from SubmitOptions import SubmitOptions
 from Step          import Step
 
+HPC_DELAY_PERIOD_SECONDS = 20
+HPC_POLL_PERIOD_SECONDS = 120
 
 class Test( SubmitAction ):
 
@@ -15,6 +18,7 @@ class Test( SubmitAction ):
   def __init__( self, name, options, defaultSubmitOptions, globalOpts, parent = "", rootDir = "./" ) :
     self.steps_         = {}
     self.masterlog_     = None
+    self.waitResults_    = False
     super().__init__( name, options, defaultSubmitOptions, globalOpts, parent, rootDir )
 
   def parseSpecificOptions( self ) :
@@ -35,7 +39,7 @@ class Test( SubmitAction ):
 
 
   def executeAction( self ) :
-    self.setupResultsStep()
+    self.checkWaitResults()
 
     steps = []
     while len( steps ) != len( self.steps_ ) :
@@ -50,67 +54,48 @@ class Test( SubmitAction ):
 
     self.postProcessResults( steps )
 
-  def setupResultsStep( self ) :
+  def checkWaitResults( self ) :
     # Do we need to add a results step?
     if self.globalOpts_.nowait :
       self.log( "No waiting for HPC submissions requested, skipping results sync" )
       return
     
-    self.log( "Checking if results sync step required" )
+    self.log( "Checking if results wait is required" )
     self.log_push()
     # Get all current steps submission type
     subs = [ step.submitOptions_.submitType_ for step in self.steps_.values() if step.submitOptions_.submitType_ != SubmitOptions.SubmissionType.LOCAL ]
     
-    if len( subs ) > 1 :
-      PBS_MIN_RES   = "1:ncpus=1"
-      SLURM_MIN_RES = "cpu:1"
-      res = None
-      submitType = None
-      if SubmitOptions.SubmissionType.PBS in subs :
-        res = PBS_MIN_RES
-        submitType = SubmitOptions.SubmissionType.PBS
-      else : # SLURM
-        res = SLURM_MIN_RES
-        submitType = SubmitOptions.SubmissionType.SLURM
-      
-      
-      if res is not None :
-        self.log( "Final results job being created to wait for all jobs complete" )
-        
-        deps = { stepname : "afterany" for stepname in self.steps_.keys() }
-        resultsDict = {
-                        "submit_options" :
-                        {
-                          "resources" : res,
-                          "wait"      : True,
-                          "submission": submitType
-                        },
-                        "command" : os.path.dirname( os.path.abspath( sys.argv[0] ) ) + "/" + "results.sh",
-                        "dependencies" : deps
-                      }
-
-        # Quickly add this step
-        self.steps_[ "results" ] = Step(
-                                        'results',
-                                        resultsDict,
-                                        self.submitOptions_,
-                                        self.globalOpts_,
-                                        parent=self.ancestry(),
-                                        rootDir=self.rootDir_
-                                        )
-        self.log( "Rescanning dependencies under test {0}...".format( self.name_ ) )
-        Step.sortDependencies( self.steps_ )
-        self.log( "Step results dependencies : {0}".format( self.steps_[ "results" ].dependencies_ ) )
-
-    elif len( subs ) == 1 :
-      self.log( "Only one HPC submission exists, setting that job to wait" )
-      for stepname, step in self.steps_.items() :
-        if step.submitOptions_.submitType_ != SubmitOptions.SubmissionType.LOCAL :
-          step.submitOptions_.wait_ = True
-          self.log( "Forcing {step} to wait for job completion".format( step=stepname ) )
+    if len( subs ) > 0 :
+      self.log( "Final results will wait for all jobs complete" )
+      self.waitResults_ = True
     else :
       self.log( "No HPC submissions, no results job added" )
 
+    self.log_pop()
+  
+  def waitOnSteps( self, stepOrder ) :
+    self.log( "Waiting for HPC jobs to finish..." )
+    self.log_push()
+    self.log( "*ATTENTION* : This is a blocking/sync phase to wait for all jobs to complete - BE PATIENT" )
+    time.sleep( HPC_DELAY_PERIOD_SECONDS )
+
+    # Filter steps already done
+    completedSteps = [ stepname for stepname in stepOrder if self.steps_[ stepname ].submitOptions_.submitType_ == SubmitOptions.SubmissionType.LOCAL ]
+
+    while len( completedSteps ) != len( stepOrder ) :
+
+      for stepname in stepOrder :
+        if stepname not in completedSteps :
+          self.steps_[ stepname ].log_push()
+          finished = self.steps_[ stepname ].checkJobComplete()
+          self.steps_[ stepname ].log_pop()
+          if finished :
+            completedSteps.append( stepname )
+
+      # Wait N seconds before checking all steps again
+      time.sleep( HPC_POLL_PERIOD_SECONDS )
+
+    self.log( "All HPC steps complete" )
     self.log_pop()
 
   def postProcessResults( self, stepOrder ) :
@@ -119,41 +104,37 @@ class Test( SubmitAction ):
     if not self.globalOpts_.nopost :
       if self.globalOpts_.nowait :
         self.log( "Post-processing requires waiting for HPC submissions, skipped" )
-      else :
-        # Get all current steps submission type
-        subs = [ step.submitOptions_.submitType_ for step in self.steps_.values() ]
+      elif self.waitResults_ :
+        # Wait for grid steps to complete
+        self.waitOnSteps( stepOrder )
 
-        if not ( SubmitOptions.SubmissionType.PBS in subs or SubmitOptions.SubmissionType.SLURM in subs ) :
-          self.log( "No HPC submissions, no results to post-process" )
-          return
-        else :
-          # We go through the steps in the order submitted
-          self.log( "Outputting results..." )
-          self.log_push()
+        # We go through the steps in the order submitted
+        self.log( "Outputting results..." )
+        self.log_push()
 
-          errLogs = {}
-          msgs    = []
-          for stepname in stepOrder :
-            if stepname != "results" and self.steps_[ stepname ].submitOptions_ != SubmitOptions.SubmissionType.LOCAL :
-              success, err = self.steps_[ stepname ].postProcessResults()
-              if not success :
-                errs = True
-                errLogs[ stepname ] = {}
-                errLogs[ stepname ][ "logfile" ] = self.steps_[ stepname ].logfile_
-                msgs.append( err )
+        errLogs = {}
+        msgs    = []
+        for stepname in stepOrder :
+          if stepname != "results" and self.steps_[ stepname ].submitOptions_ != SubmitOptions.SubmissionType.LOCAL :
+            success, err = self.steps_[ stepname ].postProcessResults()
+            if not success :
+              errs = True
+              errLogs[ stepname ] = {}
+              errLogs[ stepname ][ "logfile" ] = self.steps_[ stepname ].logfile_
+              msgs.append( err )
+        
+        if errs :
+          self.log( "Steps [ {steps} ] failed".format( steps=", ".join( errLogs.keys() ) ) )
+          self.log( "Writing relevant logfiles to view in master log file : " )
+          self.log( self.masterlog_ )
+
+          with open( self.masterlog_, "w" ) as f :
+            json.dump( errLogs, f )
           
-          if errs :
-            self.log( "Steps [ {steps} ] failed".format( steps=", ".join( errLogs.keys() ) ) )
-            self.log( "Writing relevant logfiles to view in master log file : " )
-            self.log( self.masterlog_ )
-
-            with open( self.masterlog_, "w" ) as f :
-              json.dump( errLogs, f )
-            
-            if not self.globalOpts_.nofatal :
-              raise Exception( "\n".join( msgs ) )
-          self.log_pop()
-          if not errs :
-            # We got here without errors
-            self.log( "[SUCCESS] : Test {0} completed successfully".format( self.name_ ) )
+          if not self.globalOpts_.nofatal :
+            raise Exception( "\n".join( msgs ) )
+        self.log_pop()
+        if not errs :
+          # We got here without errors
+          self.log( "[SUCCESS] : Test {0} completed successfully".format( self.name_ ) )
 
