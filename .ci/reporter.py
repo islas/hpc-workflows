804aeca81f8b97703d8ce37040a0be50c3da1d94
Anthony Islas <aislas@ucar.edu>
2023-10-30 10:15:50 -0600

    Improved reporting end message incorporating feedback from team

--- .ci/reporter.py
+++ .ci/reporter.py
@@ -2,12 +2,29 @@
 
 import json
 import sys
+import inspect
 
 def dumpFile( filename, bannerMsg="", banner="!" * 80 ) :
-  print( "\n{msg}\n{banner}".format( msg=bannerMsg, banner=banner ) )
+  print( "\nOpening logfile {0}".format( filename ) )
+  print( "{msg}\n{banner}".format( msg=bannerMsg, banner=banner ) )
   with open( filename, 'r') as f:
     print( f.read() )
   print( banner )
+
+def getLogsPrintedStr( masterDict, masterLog ) :
+  indent = "  "
+  outputFormat = "{desc:=<32}=> {file}\n"
+
+  output = outputFormat.format( desc="master log ", file=masterLog )
+  for test, testlog in masterDict.items() :
+    if not testlog["success"] :
+      output += outputFormat.format( desc=indent * 1 + "{0} stdout ".format( test ), file=testlog["message"] )
+      for step, steplog in testlog["steps"].items() :
+        if not steplog["success"] :
+          output += outputFormat.format( desc=indent * 2 + "{0} stdout ".format( step ), file=steplog["logfile"] )
+  
+  return output
+
 
 masterLog = sys.argv[1]
 fp = open( masterLog )
@@ -24,9 +41,18 @@ for test, testlog in logs.items() :
       if not steplog["success"] :
         print( "Step {step} failed, printing stdout".format( step=step ) )
         dumpFile( steplog["logfile"], bannerMsg="STDOUT FOR STEP {step}".format( step=step ) )
-    
+
     print( "\n".join([( "#" * 80 )]*3 ) )
 
 # Exit with bad status so people know where to look since that might be 
 # an issue as this will look "successful"
-raise Exception( "Test did not pass, refer to the above logs" )
+refLogs  = getLogsPrintedStr( logs, masterLog )
+howToUse = inspect.cleandoc(
+           """~ How to use brief ~
+                Search for (remove single quotes): 
+                  * '!!!!!!!!!!!' (up to 80) to find beginning and end of a step
+                  * '###########' (up to 80) to find beginning of a test
+                  * '!!!!!!!!!! ERROR ERROR ERROR !!!!!!!!!!' to find test infrastructure reason for failure
+           """ )
+
+raise Exception( "Test did not pass, refer to the printed logs above using :\n{help}\n\nOr refer to log files : \n{reflogs}".format( help=howToUse, reflogs=refLogs ) )
